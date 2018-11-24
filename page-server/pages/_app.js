74139d7fd536862c106ab7b61c40f3218a725b59
chrisfield <chris@christopherfield.co.uk>
2018-11-24 03:33:14 +0000

    Add redux and saga and 'other' page with clock as an example

--- page-server/pages/_app.js
+++ page-server/pages/_app.js
@@ -1,17 +1,41 @@
-import App, {Container} from 'next/app'
-import React from 'react'
-import withApolloClient from '../lib/with-apollo-client'
-import { ApolloProvider } from 'react-apollo'
+import App, {Container} from 'next/app';
+import React from 'react';
+import withApolloClient from '../lib/with-apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+
+import { Provider as ReduxProvider } from 'react-redux';
+import withRedux from 'next-redux-wrapper';
+import withReduxSaga from 'next-redux-saga';
+
+import createStore from '../store';
+
 
 class MyApp extends App {
+  static async getInitialProps ({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps({ ctx })
+    }
+
+    return { pageProps }
+  }
+
   render () {
-    const {Component, pageProps, apolloClient} = this.props
+    const {Component, pageProps, apolloClient, store} = this.props
     return <Container>
-      <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <ReduxProvider store={store}>
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ReduxProvider>
     </Container>
   }
 }
 
-export default withApolloClient(MyApp)
+export default withRedux(createStore)(
+  withReduxSaga({ async: true })(
+    withApolloClient(MyApp)
+  )
+);
